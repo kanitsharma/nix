79d645438c19568acf4559c8cf76859a82812553
kanitsharma <kanitsharma@protonmail.com>
2022-10-08 19:54:53 +0530

    First commit

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+set fish_greeting
+
+starship init fish | source
+mcfly init fish | source
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    if test "$KITTY_AUTOSTART" = "next"
+        fish -c 'yarn dev; fish'
+    end
+    if test "$KITTY_AUTOSTART" = "golang"
+        fish -c 'go run .; fish'
+    end
+
+end
+# source ~/.iterm2_shell_integration.fish
---
+++ home.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "kanitsharma";
+  home.homeDirectory = "/Users/kanitsharma";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.packages = [
+    pkgs.nodejs
+    pkgs.fish
+    pkgs.go
+  ];
+
+  programs.fish = {
+    enable = true;
+    shellInit = builtins.readFile ./fish/config.fish;
+    plugins = [
+      {
+        name = "nix-env";
+        src = pkgs.fetchFromGitHub {
+          owner = "lilyball";
+          repo = "nix-env.fish";
+          rev = "00c6cc762427efe08ac0bd0d1b1d12048d3ca727";
+          sha256 = "1hrl22dd0aaszdanhvddvqz3aq40jp9zi2zn0v1hjnf7fx4bgpma";
+        };
+      }
+    ];
+  };
+}
